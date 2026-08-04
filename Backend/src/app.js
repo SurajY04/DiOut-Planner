a642d2eef9ad925620ea641d618b074187fa8167
SurajY04 <suraj04uy@gmail.com>
2026-08-04 23:39:27 +0530

    .

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin : "https://hilarious-torte-46be6d.netlify.app/" ,
+    origin : "https://hilarious-torte-46be6d.netlify.app" ,
     credentials : true
 }))
 
